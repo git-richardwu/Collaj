446f1919c03a234fba36bcf4ac3789d0e9efd47b
git-richardwu <richardwu720@gmail.com>
2022-11-22 21:43:58 -0500

    updated Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,39 @@
 import { Link } from "react-router-dom";
-import './navbar.css';
+import '../index.css';
+import { useUserContext } from "../hooks/useUserContext";
+import { useLogout } from "../hooks/useLogout";
 
 export default function Navbar() {
+    const { logout } = useLogout();
+    const { user } = useUserContext();
+
+    const handleLogout = () => {
+        logout();
+    }
+    
     return (
         <div className="navbar-style">
-            <Link to="/">
-                <h2>Collaj</h2>
-            </Link>
+            <div className="name">
+                    <Link to="/">
+                        <h2>Collaj</h2>
+                    </Link>
+            </div>
+            {user && (
+                <div className="separate">
+                    <h4 className="welcome">Welcome, {user.username}</h4>
+                    <button className="logout" onClick={handleLogout}>Log out</button>
+                </div>
+            )}
+            {!user && (
+            <div className="separate">
+                    <Link to="/signup">
+                        <h3>Sign Up</h3>
+                    </Link>
+                    <Link to="/login">
+                        <h3>Login</h3>
+                    </Link>
+            </div>
+            )}
         </div>
     )
 }
