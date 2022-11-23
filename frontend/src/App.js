6bd43eb92084225d374842d320af42a1b67f22ca
git-richardwu <richardwu720@gmail.com>
2022-11-22 22:08:12 -0500

    Updated index and app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/HomePage';
 import ArtPage from './pages/ArtPage';
 import Navbar from './components/Navbar';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import { useUserContext } from './hooks/useUserContext';
 
 function App() {
+  const { user } = useUserContext();
+
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />}/>
+            <Route path="/" element={user ? <Home /> : <Navigate to="/login"/>}/>
             <Route exact path="/art-details/:id"
-              element={<ArtPage />}
+              element={user ? <ArtPage /> : <Navigate to="/"/>}
             />
+            <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to="/"/>}/>
+            <Route path="/login" element={!user ? <LoginPage />: <Navigate to="/"/>}/>
           </Routes>
         </div>
       </BrowserRouter>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ArtContextProvider } from './components/context/ArtContext';
+import { ArtContextProvider } from './context/ArtContext';
+import { UserContextProvider } from './context/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
+    <UserContextProvider>
       <ArtContextProvider>
         <App />
       </ArtContextProvider>
+    </UserContextProvider>
   </React.StrictMode>
 );
 
+ 
