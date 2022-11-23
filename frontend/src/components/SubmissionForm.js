4236ea0f2936e48ab5fe4ab95df7b9396cd3cf85
git-richardwu <richardwu720@gmail.com>
2022-11-22 21:56:33 -0500

    Update SubmissionForm.js

--- frontend/src/components/SubmissionForm.js
+++ frontend/src/components/SubmissionForm.js
@@ -1,50 +1,64 @@
 import { useState } from 'react';
-import { useArtContext } from "./hooks/useArtContext";
+import { useArtContext } from "../hooks/useArtContext";
+import { useUserContext } from '../hooks/useUserContext';
 
 export default function SubmissionForm() {
-    const { dispatch } = useArtContext()
-    const [source, setSource] = useState('')
-    const [pieceIndex, setIndex] = useState(1)
-    const [error, setError] = useState(null)
-    const [isLoading, setLoading] = useState(false)
-
+    const { dispatch } = useArtContext();
+    const { user } = useUserContext();
+    const [source, setSource] = useState('');
+    const [pieceIndex, setIndex] = useState(1);
+    const [error, setError] = useState(null);
+    const [artSite, setArtSite] = useState("Artstation");
+    const [isLoading, setLoading] = useState(false);
+    
     const handleSubmit = async (e) => {
-        setLoading(true)
-        e.preventDefault()
-        const art = { source, pieceIndex }
+        setLoading(true);
+        e.preventDefault();
+        if (!user) {
+            setError('You have to be logged in!');
+            return;
+        }
+        const art = { source, pieceIndex, artSite };
         const response = await fetch('/api/art', {
             method: 'POST',
             body: JSON.stringify(art),
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${user.token}`
             }
         })
-        const json = await response.json()
-        if (response.ok){
-            setSource('')
-            setIndex(1)
-            setError(null)
-            console.log('art added')
-            setLoading(false)
-            dispatch({type: 'ADD_ARTWORK', payload: json})
+        const json = await response.json();
+        if (response.ok) {
+            setSource('');
+            setIndex(1);
+            setError(null);
+            setLoading(false);
+            dispatch({type: 'ADD_ARTWORK', payload: json});
         }
         else {
-            setLoading(false)
-            setError(json.error)
-            // console.log(error)
-            // console.log(art)
+            setLoading(false);
+            setError(json.error);
         }
     }
     return (
         <form className='add' onSubmit={handleSubmit}>
             <h2>Add new artwork!</h2>
-            <label> Artwork URL: </label>
-            <input type="text" onChange={(e) => setSource(e.target.value)} value={source}/>
-            <label> Index: </label>
-            <input type="number" min="1" max="50" onChange={(e) => setIndex(e.target.value)} value={pieceIndex}/>
-            <button>Add</button>
+            <div className="formGroup">
+                <label> Artwork URL: </label>
+                    <input type="text" className="urlForm" onChange={(e) => setSource(e.target.value)} value={source}/>
+            </div>
+            <div className="formGroup">
+                <label> Index: </label>
+                    <input type="number" className="indexForm" min="1" max="50" onChange={(e) => setIndex(e.target.value)} value={pieceIndex}/>
+                <label> Site: </label>
+                    <select value={artSite} onChange={(e) => setArtSite(e.target.value)} id="site" name="site">
+                        <option value="Artstation">Artstation</option>
+                        <option value="Twitter">Twitter</option>
+                    </select>
+                    <button>Add</button>
+            </div>
             { isLoading && <div>Loading...</div>}
-            { error && <div className="errorMessage">Invalid Path. Try adjusting the index since ArtStation's indexing can be inconsistent.</div>}
+            { error && <div className="errorMessage">Index provided is out of bounds.</div>}
         </form>
     )
 }
